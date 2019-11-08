f8695828c581daa94585d8a691350e4d9250f7ff
asai takayuki <achaikunn@gmail.com>
2019-11-08 19:02:16 +0900

    [overview] update HTML/CSS project and execise in 10
    [details]nothing

---
+++ PassByReference-intro/script.js
@@ -0,0 +1,51 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+// Basic Requirements
+// 1
+//
+//let a="June 6";
+//
+//const b=a;
+//
+//a +="is my mother's birthday.";
+//
+//console.log(a); //should print "June 6 is my mother's birthday."
+//console.log(b); //should print "June 6"
+//
+//2.I want to hold your hand
+// 
+//
+//const theBeatles ={
+//    bass: ["paul"],
+//    drum: ["ringo"],
+//    guitar: ["george","john"],
+//    vocals:["george","john","paul","ringo"]
+//};
+//
+//const theWannabes = theBeatles;
+//theWannabes.bass.push("someone else");
+//
+//console.log(theBeatles.bass); //should be print ["paul","someone else"]
+//console.log(theWannabes.bass);//should be print ["paul","someone else"]
+//
+//3.The best day of the week...
+//
+//const a ="Sunday"; //this is primitive type
+//let b = a; //primitive type
+//
+//a += " is the best day of the week.";
+//
+//console.log(a); //should be print "Sunday is the best of the week."
+//console.log(b); //should be print "Sunday".Because this is passed by value.
+//4.A rainbow.
+//const x = ["red","orange","yellow","green","blue","purple"];
+//const y = x.slice(3);
+//y[2] = "pink";
+//
+//console.log(x); //should print ["red","orange","yellow"]
+//console.log(y); //should print 
+
+//5Another slice.
+
+
