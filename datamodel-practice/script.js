ea763a40193d7bf648a37be66fc09de414611834
asai takayuki <achaikunn@gmail.com>
2019-11-12 17:59:16 +0900

    basic requirement in data modeling

--- datamodel-practice/script.js
+++ datamodel-practice/script.js
@@ -49,28 +49,70 @@ Books and sheffs project
 
 //create a books object
 
-const book1 = {
-    title: 'Harry Potter',
-    author: 'JK Rowling',
-    totalPages: 700,
-    pageRead: 400,
-    timeRead: 3
-};
-const book2 = {
-    title: 'Lord of the Prings',
-    author: 'JRR Tolkien',
-    totalPages:500,
-    pageRead: 0,
-    timeRead: 0
-};
-const book3 = {
-    title: 'Norwegian Wood',
-    author: 'Haruki Murakami',
-    totalPages:296,
-    pageRead: 296,
-    timeRead: 2
-};
+//const book1 = {
+//    title: 'Harry Potter',
+//    author: 'JK Rowling',
+//    totalPages: 700,
+//    pageRead: 400,
+//    timeRead: 3
+//};
+//const book2 = {
+//    title: 'Lord of the Prings',
+//    author: 'JRR Tolkien',
+//    totalPages:500,
+//    pageRead: 0,
+//    timeRead: 0
+//};
+//const book3 = {
+//    title: 'Norwegian Wood',
+//    author: 'Haruki Murakami',
+//    totalPages:296,
+//    pageRead: 296,
+//    timeRead: 2
+//};
+//
+//console.log(book1);
+//console.log(book2);
+//console.log(book3);
 
-console.log(book1);
-console.log(book2);
-console.log(book3);
+//Basic Requirement
+//Like we did with books, think about what kinds of aspects of movies you would like to represent. A few ideas are:
+//2.Write a factory function makeMovie that takes in inputs and outputs a movie object.
+function makeMovie(title,director,year,times){
+    return {
+        title: title,
+        director: director,
+        year: year,
+        times: times,
+    }
+}
+
+const movie1 = makeMovie("norwegian wood","haruki murakami","1990",120);
+const movie2 = makeMovie("norwegian wood","haruki murakami","1990",10);
+const movies = [movie1,movie2];
+
+//4.Declare a function displayMovie that works like displayBook, but for movies.
+//function displayMovie(object){
+//    console.log(object.title + "'s directory is  " + object.director);
+//}
+
+//how to maniplate object...
+function displayMovie(object){
+    console.log(object['title'] + "'s directory is  " + object['director']);
+}
+
+//output a messages to the console...
+displayMovie(movie1);
+
+// 5.Calculate the average length of your movies by writing a function called averageLength that will accept an array of movies as a parameter and output the average length. The difficulty of this problem is dependent on how you have chosen to store the duration.
+function averageLength(movieObj){
+        let averageLength = 0;
+    for(let i = 0;i <= movieObj.length;i++){
+        averageLength += movieObj[i]['times'];
+    }
+        return averageLength;
+}
+
+console.log(averageLength(movies));
+
+
