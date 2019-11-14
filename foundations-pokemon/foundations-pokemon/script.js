fa23b16b903763f2b07ad96a3976333490de1e2d
asai takayuki <achaikunn@gmail.com>
2019-11-14 17:44:36 +0900

    20191111 basic requirement in closer pokemon

---
+++ foundations-pokemon/foundations-pokemon/script.js
@@ -0,0 +1,56 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+//TEST
+function test(actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log("Yay! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Keep trying!");
+    console.log("    actual: ", actual);
+    console.log("  expected: ", expected);
+    console.trace();
+  }
+}
+
+
+
+/**
+ *  1.
+ *  Print allPokemon to your Chrome console to see what it looks like. (This is already done for you.)
+ */
+//console.log(allPokemon)
+
+ /**
+  * Print the length.
+  */
+
+//console.log(allPokemon.length)
+
+  /**
+   * 3.Print out the first Pokemon and look at what a Pokemon objec looks like.
+   */
+
+console.log(allPokemon[0])
+
+
+/**
+ * Basic Requirements
+ */
+
+ /**
+  * 1.Declare a function getNames that returns an array of all the name of Pokemon in the data set.
+  */
+
+function getNames(){
+    let allPokemonName = [];
+    
+    for(let i=0; i < allPokemon.length; ++i){
+        let pokemonName = allPokemon[i].Name;
+        allPokemonName.push(pokemonName);
+    }
+    
+    return allPokemonName;
+}
+
+console.log(getNames()); //should be print "Bulbasaur"??
