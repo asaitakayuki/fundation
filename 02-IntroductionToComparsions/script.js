d9728e74acd8e9a4959d373b59ed4786df9361e7
asai takayuki <achaikunn@gmail.com>
2019-12-03 15:37:21 +0900

    I did basic requirement in 02

---
+++ 02-IntroductionToComparsions/script.js
@@ -0,0 +1,82 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+/**
+ * Basic Requirement
+ */
+
+ /**
+  * 1.Before running the code below, what do you think the expressions will resolve to? Try them in the console to see if you are correct!
+  */
+
+//  console.log("true" == true);//It is wired...I guess it returns false.
+//  console.log("true" === true); //It is diffrence type of data."true" is string,true is boolean. false
+//  console.log(3 >= 3);// 3 is equal to 3.So,it returns true.
+//  console.log(3 != 4);// 3 is not equal to 4.true
+
+/**
+ * 2.Copy the code into your script.js file and fill in the ??? with the appropriate comparison operators or values to make the statements output the expected boolean value. Open index.html in Chrome and then open the Developer Console to check the results.
+ */
+//console.log(1100 !== 99) // should return true
+//
+//console.log(1 > 21) // => false
+//
+//console.log(62 !== true) // => true
+//
+//console.log("5" === 5) // => false
+//
+//console.log("6" !== "six") // => true
+
+/**
+ * Copy the code into your script.js file and change ONE of the ARITHMETIC operators in the expression below so that it evaluates to true. Make sure you understand the ORDER that the expressions evaluate in.
+ */
+
+
+// oritinal console.log(2 + 3 * 10 > 50);
+
+//console.log(2 * 3 * 10 > 50);
+
+
+/**
+ * 4.Add the function below to your script.js file and invoke it by replacing ??? with two DIFFERENT inputs so that the expression evaluates to true. Remember, === checks the value and type, but == only checks the value.
+ */
+
+// function isEqual(valueOne,valueTwo){
+//    return valueOne == valueTwo;
+// }
+//
+// console.log(isEqual(1,"1"));
+
+/**
+ * 5.Add the function below to your script.js file and write a statement that returns true when valueOne is MORE than valueTwo.
+ */
+
+//function isGreterThan(valueOne,valueTwo){
+//    if (valueOne > valueTwo){
+//        return true;
+//    }else{
+//        return false;
+//    }
+//}
+//
+//console.log(isGreterThan(1,2)); //false
+//console.log(isGreterThan(2,2)); //false
+//console.log(isGreterThan(3,2)); //true
+//
+
+
+/**
+ * 6.In japan,you can drink alcol if you are aged 20 or older..etcetc
+ */
+
+//function isOfAge(number){
+//    if(number >= 20){
+//        return true;
+//    }else{
+//        return false;
+//    }
+//}
+//
+//console.log(isOfAge(19)); //false
+//console.log(isOfAge(20)); //true
+//console.log(isOfAge(21)); //true
--- 02-introductionToFunctions/script.js
+++ 02-introductionToFunctions/script.js
@@ -37,5 +37,98 @@
  * 3.What do you think gets printed when you run the following code? Try to predict what happens and then see if you can figure out why. The extra sentence that we are printing out in the two console.log statements at the end will help you figure out what is happening where. Scroll to the explanation below to find the answer.
  */
 
+//function simpleFunctionA(){
+//  return "Hello simple function A";
+//}
+//
+//function simpleFunctionB(){
+//  console.log("Hello simple function B");
+//}
+//
+//console.log(simpleFunctionA(),"--> From running Simple FunctionA");
+//console.log(simpleFunctionB(),"--> From running Simple FunctionB");
 
- 
+
+/**
+ * 4.Declare a function named subtract that subtracts the second argument from the first argument. Remember to try the test cases to see if your function works.
+ */
+
+// function substract(num1,num2){
+//   return num1 - num2;
+// }
+//
+//
+// //TEST
+// console.log(substract(4,3));
+// console.log(substract(100,42));
+
+/**
+ * 5.Declare a function named greeting that takes a name string as an argument and returns a hello!
+ */
+
+
+// function greeting(name){
+//   return "Hello, " + name;
+// }
+//
+//
+// console.log(greeting("Alex"));
+// console.log(greeting("Beau"));
+
+
+/**
+ * Declare a function called average that takes two numbers as inputs and returns the average of those numbers. This time, write two tests for your function by yourself!
+ */
+
+// function average(num1,num2){
+//   return (num1 + num2 ) / 2;
+// }
+//
+// console.log(average(1,1)) //should be 1
+// console.log(average(10,5)) //should be 2
+
+/**
+ * 7.What is wrong with the following functions?
+ */
+
+
+/**
+ * parameter is a number.Parameter should be variable
+ *
+ *  */
+//function square(5){
+//  return 5 * 5;
+//}
+//
+//
+///**
+// * parameter is expected a number.But This prameter is string.
+// * So,This function can not square the number.
+// */
+//
+//function square("x"){
+//  return "x" * "x";
+//}
+//
+
+/**
+ * 8.Write a function called cube that returns the cube of x
+ */
+
+// function cube(x){
+//   return x * x * x;
+// }
+
+
+//Why are we printing an undefined in the below code?
+
+//function simpleFunctionA() {
+//  return "Hello simple function A";
+//}
+//
+//function simpleFunctionB() {
+//  console.log("Hello simple function B");
+//}
+//
+//console.log(simpleFunctionA(), "--> From running Simple Function A");
+//console.log(simpleFunctionB(), "--> From running Simple Function B");
