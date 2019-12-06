53e9320ea3ba54bb45fddd65729665a420f8e641
asai takayuki <achaikunn@gmail.com>
2019-12-06 15:50:48 +0900

    20191209 backup

---
+++ 03_Warmup(FunctionsIntro)/script.js
@@ -0,0 +1,17 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+//function greeting(name){
+//    return "Hello, " + name + "!";
+//}
+//
+//console.log(greeting("Kana"));
+//console.log(greeting("kimiko"));
+//
+//function average(number1,number2){
+//    return (number1 + number2)/2;
+//}
+//
+//console.log(average(1,1))// 1
+//console.log(average(5,1))// 3
+//
